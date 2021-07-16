f180c4edbe2a37b55fdac5b0d9d5354d12f197fb
yuru <yurutung@gmail.com>
2021-07-16 15:10:50 +0800

    add css

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
+// css
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 ReactDOM.render(<App />, document.getElementById("root"));
